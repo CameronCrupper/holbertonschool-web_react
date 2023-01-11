6c99ecd32bedcddaa7bc7cf9b9105350306d89d6
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-11 17:08:59 -0600

    test

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,32 +1,31 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
-  
   module: {
     rules: [
-			{
-        test: /\.css$/i,
-        use: ['style-loader','css-loader']
-      },
-
       {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        type: 'asset/resource',
+        test: /\.css$/,
+        use: [  'style-loader', 'css-loader' ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ],
-  },
-  
-  mode: "production",
-
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-}
-
+  }
 };
