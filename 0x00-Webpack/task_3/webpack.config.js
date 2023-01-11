3b91c9dec44fab0d422d606369d7638864bf9483
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-11 17:22:33 -0600

    test

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,60 +1,55 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: {
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js'
+    all: [
+      './modules/header/header.js',
+      './modules/body/body.js',
+      './modules/footer/footer.js',
+    ]
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    clean: true,
+    path: path.resolve(__dirname, 'public')
   },
-
-  optimization: {
-    runtimeChunk: 'single',
-  },
-
-  plugins:
-   [
-    new HtmlWebpackPlugin(),
-    new CleanWebpackPlugin(),
-  ],
-
-  
-  devtool: "inline-source-map",
-
   module: {
     rules: [
-			{
-        test: /\.css$/i,
-        use: ['style-loader','css-loader']
-      },
-
       {
-        test: /\.(png|svg|jpe?g|gif)$/i,
-        type: 'asset/resource',
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
-    ],
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            },
+          },
+        ],
+      },
+    ]
   },
-  
-  mode: "development",
-
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-},
-
-devServer: {
-  static: {
-    directory: path.join(__dirname, 'public'),
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564
   },
-  compress: true,
-  port: 8564,
-},
+  plugins: [
+    new CleanWebpackPlugin(), // clean the public folder before building
+    new HtmlWebpackPlugin() // Generates default index.html
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all' // split all chunks
+    }
+  }
 
 };
