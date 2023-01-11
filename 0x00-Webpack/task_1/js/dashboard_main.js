e01b7695789c80a4931cb1795de0f1ec77125760
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-11 16:47:28 -0600

    test

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,11 +3,13 @@ import _ from 'lodash';
 
 $("<p>Holberton Dashboard</p>")["$('body').append"];
 $("<p>Dashboard data for the students</p>")["$('body').append"];
-$("<p>Copyright - Holberton School</p>")["$('body').append"];
+$('body').append('<button>Click here to get started</button>');
 $("<p id='count'></p>")["$('body').append"];
 $("<p>Copyright - Holberton School</p>")["$('body').append"];
 
-let count = 0;
-const updateCounter = () => $('#count').html(`${++count} clicks on the button`);
-
-$('button').on('click', _.debounce(updateCounter, 500, { 'leading': true }));
+let counter = 0;
+const debouncedCounter = _.debounce(() => {
+    counter++;
+    $("#count").text(`${counter} clicks on the button`);
+});
+$('button').on('click', debouncedCounter);
