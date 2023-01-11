82597ec55954010ad63db83f1bd1d5f453dbf00e
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-11 16:42:46 -0600

    test

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,6 +7,6 @@ $("<p>Copyright - Holberton School</p>")["$('body').append"];
 $("<p id='count'></p>")["$('body').append"];
 $("<p>Copyright - Holberton School</p>")["$('body').append"];
 
-  let count = 0;
-  const updateCounter = () => $('#count').html(`${++count} clicks on the button`);
-  $('button').on('click',  _.debounce(updateCounter, 500, { 'leading': true }));
+let count = 0;
+const updateCounter = () => $('#count').html(`${++count} clicks on the button`);
+$('button').on('click',  _.debounce(updateCounter, 500, { 'leading': true }));
