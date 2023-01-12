de0eeecb73833d3916ccf599decac22e26b1f0ca
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-11 21:52:23 -0600

    fix webpack config

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,57 +1,47 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
-    },
-    module: {
-        rules: [
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    compress: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          'file-loader',
           {
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader'],
-          },
-
-          {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            type: "asset",
-          },
-
-          {
-            test: /\.(jsx|js)$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
           },
         ],
-    },
-
-    devServer: {
-      contentBase: path.resolve(__dirname, 'dist'),
-      port: 8564,
-      hot: true
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-          title: 'Hot Module Replacement',
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', ['@babel/preset-react', {'runtime': 'automatic'}]],
+          },
+        },
+      },
     ],
-    
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    mode: 'development'
-}
+  },
+};
