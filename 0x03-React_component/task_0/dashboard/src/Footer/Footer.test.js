9960330ba0ee72a6de47a40350dc3adbe7889e83
Cameron Crupper <guitardude20795@yahoo.com>
2023-01-18 14:29:18 -0600

    test

--- 0x03-React_component/task_0/dashboard/src/Footer/Footer.test.js
+++ 0x03-React_component/task_0/dashboard/src/Footer/Footer.test.js
@@ -1,10 +1,21 @@
-import { shallow } from "enzyme";
-import React from "react";
-import Footer from "./Footer";
+import Footer from './Footer.js'
+import { shallow } from 'enzyme';
+import React from 'react';
+import "../../config/setupTests"
+import { expect } from 'chai';
 
-describe("<Footer />", () => {
-  it("Footer renders without crashing", () => {
-    const wrapper = shallow(<Footer />);
-    expect(wrapper.exists()).toEqual(true);
-  });
+
+describe("Testing the <Footer /> componet", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Footer />)
+    })
+    it("Footer componet loads", () => {
+        expect(wrapper.render()).to.not.be.an("undefined")
+    });
+
+    it("Footer componet has text", () => {
+        expect(wrapper.text().includes('Copyright')).to.equal(true);
+    });
 });
--- 0x03-React_component/task_0/dashboard/src/Login/Login.test.js
+++ 0x03-React_component/task_0/dashboard/src/Login/Login.test.js
@@ -1,10 +1,25 @@
-import { shallow } from "enzyme";
-import React from "react";
-import Login from "./Login";
+import Login from './Login.js'
+import { shallow } from 'enzyme';
+import React from 'react';
+import "../../config/setupTests"
+import { expect } from 'chai';
 
-describe("<Login />", () => {
-  it("Login renders without crashing", () => {
-    const wrapper = shallow(<Login />);
-    expect(wrapper.exists()).toEqual(true);
-  });
+
+describe("Testing the <Login /> componet", () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<Login />)
+    })
+    it("Login componet loads", () => {
+        expect(wrapper.render()).to.not.be.an("undefined")
+    });
+
+    it("Login componet has img", () => {
+        expect(wrapper.find("input")).to.have.lengthOf(2);
+    });
+
+    it("Login componet has h1", () => {
+        expect(wrapper.find("label")).to.have.lengthOf(2);
+    });
 });
